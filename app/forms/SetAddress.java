ffb870ff922e76eb8358456e530e2bf0ee71bbac
Laura Luiz <laura.luiz@commercetools.de>
2013-09-06 13:49:17 +0200

    Remove unused form fields.

--- app/forms/SetAddress.java
+++ app/forms/SetAddress.java
@@ -10,26 +10,14 @@ import java.util.List;
 
 public class SetAddress {
 
-    public String company;
-
     @Constraints.Required(message = "First name required")
     public String firstName;
 
     @Constraints.Required(message = "Last name required")
     public String lastName;
 
-    @Constraints.Email(message = "Invalid value for email")
-    public String email;
-
-    @Constraints.Required(message = "Phone required")
-    public String phone;
-
-    public String mobile;
-
     @Constraints.Required(message = "Street address required")
     public String street;
-
-    public String street2;
 
     @Constraints.Required(message = "Postal code required")
     public String postalCode;
@@ -38,7 +26,7 @@ public class SetAddress {
     public String city;
 
     @Constraints.Required(message = "Country required")
-    @Constraints.Pattern(value = "DE|AT", message = "Invalid value for country")
+    @Constraints.Pattern(value = "DE", message = "Invalid value for country")
     public String country;
 
     public SetAddress() {
@@ -47,36 +35,31 @@ public class SetAddress {
 
     public SetAddress(io.sphere.client.shop.model.Address address) {
         if (address != null) {
-            this.company = address.getCompany();
             this.firstName = address.getFirstName();
             this.lastName = address.getLastName();
-            this.email = address.getEmail();
-            this.phone = address.getPhone();
-            this.mobile = address.getMobile();
             this.street = address.getStreetName();
-            this.street2 = address.getStreetNumber();
             this.postalCode = address.getPostalCode();
             this.city = address.getCity();
             this.country = address.getCountry().getAlpha2();
+        } else {
+            this.firstName = "Chief";
+            this.lastName = "Wiggum";
+            this.street = "Evergreen Terrace, 13";
+            this.postalCode = "58331";
+            this.city = "Springfield";
+            this.country = "DE";
         }
     }
 
     public SetAddress(Customer customer) {
-        this(null, customer.getName().getFirstName(), customer.getName().getLastName(), customer.getEmail(), null,
-                null, null, null, null, null, null);
+        this(customer.getName().getFirstName(), customer.getName().getLastName(), null, null, null, null);
     }
 
-    public SetAddress(String company, String firstName, String lastName, String email, String phone,
-                      String mobile, String street, String street2,
+    public SetAddress(String firstName, String lastName, String street,
                       String postalCode, String city, String country) {
-        this.company = company;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.mobile = mobile;
         this.street = street;
-        this.street2 = street2;
         this.postalCode = postalCode;
         this.city = city;
         this.country = country;
@@ -84,14 +67,9 @@ public class SetAddress {
 
     public Address getAddress() {
         Address address = new Address(getCountryCode());
-        address.setCompany(company);
         address.setFirstName(firstName);
         address.setLastName(lastName);
-        address.setEmail(email);
-        address.setPhone(phone);
-        address.setMobile(mobile);
         address.setStreetName(street);
-        address.setStreetNumber(street2);
         address.setPostalCode(postalCode);
         address.setCity(city);
         return address;
@@ -104,7 +82,6 @@ public class SetAddress {
     public static List<CountryCode> getCountryCodes() {
         List<CountryCode> countries = new ArrayList<CountryCode>();
         countries.add(CountryCode.DE);
-        countries.add(CountryCode.AT);
         return countries;
     }
 }
